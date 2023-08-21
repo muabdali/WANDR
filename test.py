8d23e04cd70787a5161da28d938b8b17b49bd3bd
Muslum Abd Ali <muslumabdali1234@gmail.com>
2023-08-20 23:25:27 -0400

    finished getting typing

--- test.py
+++ test.py
@@ -1,1 +1,21 @@
-#TEST
+import pandas as pd
+import csv
+import json
+
+class wandrMain():
+    def __init__(self):
+        self.gen3TypingChart = 'data/typingChart.csv'
+    
+    
+    def WANDR(self, pokemonName):
+        self.getTyping(pokemon=pokemonName)
+        
+        
+    def getTyping(self, pokemon):
+        with open('data/pokeTypeMasterSheet.json') as file:
+            data = json.load(file)
+        for pokemonName in data:
+            if pokemonName["Name"].lower() == pokemon.lower():
+                types = pokemonName["Type1"] + pokemonName["Type2"]
+                return types
+            print(types)
